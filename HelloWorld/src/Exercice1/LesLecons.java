5dba1df68b3e3f96990c129c437c04f3f05c0832
Rems6T <remi.tidda@gmail.com>
2022-11-07 10:40:45 +0100

    Récupération des saisies de l'utilisateur
    Il faut importer la classe "Scanner" qui est une classe native de Java.

--- HelloWorld/src/Exercice1/LesLecons.java
+++ HelloWorld/src/Exercice1/LesLecons.java
@@ -1,5 +1,8 @@
 package Exercice1;
 
+// On importe une classe de Java qui permet de lire les saisies de l'utilisateur
+import java.util.Scanner;
+
 public class LesLecons {
 
 //	Déclaration d'une constante
@@ -8,17 +11,25 @@ public class LesLecons {
 	public static void main(String[] args) {
 //		Déclaration d'une variable
 		double prixHT = 17.85;
+		System.out.printf("Prix TTC de l'article : %.2f€%n", prixHT*(1+TVA));
+		
+		
 		
 //		Déclaration d'un tableau de taille 100
 		float[] valeurs = new float[100];
-//		Ecriture dans la 7eme case du tableau
+//		Ecriture dans la 8eme case du tableau
 		valeurs[7] = 1792;
 //		Lecture d'une valeur du tableau
 		System.out.println(valeurs[7]);
 		
-		System.out.printf("Prix TTC de l'article : %.2f€%n", prixHT*(1+TVA));
 		
 		
+//		Création de la variable qui va lire les saisies de l'utilisateur
+		Scanner s = new Scanner(System.in);
+		System.out.println("Quel est votre prénom ? ");
+//		On récupère une saisie en String (nextInt pour les nombres, nextFloat pour les nombres à virgule, etc...)
+		String prenom = s.nextLine();
+		s.close();
 	}
 
 }
